a56145216993ca8c1c98183aa457fafedba2286b
Brian Kim <bkim8815@gmail.com>
2022-01-05 13:43:16 -0500

    train test

---
+++ starter/test_train_model.py
@@ -0,0 +1,41 @@
+# Script to train machine learning model.
+
+# Add the necessary imports for the starter code.
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from starter.ml.data import process_data
+from starter.ml.model import train_model
+import joblib
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data():
+    return pd.read_csv("./data/census_clean.csv")
+
+
+
+
+def test_train_model(data):
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder, lb = process_data(
+        data, categorical_features=cat_features, label="salary", training=True
+    )
+    model = train_model(X_train, y_train)
+    assert model != None
+
+
+def test_train_test_split(data):
+    train, test = train_test_split(data, test_size=0.20)
+    assert test.shape[0] > 0
